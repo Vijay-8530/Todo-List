0f692248b0990469e3f668de310f702ddd6a6f5d
vijay-8530 <vijaysangani2015@gmail.com>
2023-04-27 19:22:31 +0530

    complet project

---
+++ script.js
@@ -0,0 +1,44 @@
+const addTodo = document.querySelector('.add')
+const list = document.querySelector('.todos')
+const search = document.querySelector('.search input')
+// console.log(search)
+const generatemplet = todo => {
+    const html = `<li class="list-group-item d-flex justify-content-between align-items-center">
+    <span>${todo}</span>
+    <i class="far fa-trash-alt delet"></i>
+    </li>`
+    list.innerHTML += html
+}
+addTodo.addEventListener('submit', e => {
+    e.preventDefault()
+    const todo = addTodo.add.value.trim();
+    if (todo.length) {
+        generatemplet(todo)
+        addTodo.reset()
+    }
+})
+// delet todos
+list.addEventListener('click', e => {
+    console.log(e)
+    if (e.target.classList.contains('delet')) {
+        e.target.parentElement.remove()
+    }
+})
+// search todos
+
+const filterTodos = term => {
+    console.log(term)
+    Array.from(list.children)
+        .filter(e => !e.textContent.includes(term))
+        .forEach(e => e.classList.add('filtered'))
+    // console.log(Array.from(list.children))
+    Array.from(list.children)
+        .filter(e => e.textContent.includes(term))
+        .forEach(e => e.classList.remove('filtered'))
+}
+
+search.addEventListener('keyup', () => {
+    const term = search.value.trim()
+    // console.log(term)
+    filterTodos(term)
+})
